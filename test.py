4188e5673c4f0cf6ad597fa27e71eb0d00c2c926
danhuang <danhuang0728@gmail.com>
2024-07-05 05:54:03 +0800

    main

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-a = 1
-print(a)
+nums = [1]
+target = 0
+def searchInsert(nums, target):
+    for i in range(len(nums)):
+        if nums[i] == target :
+            return i
+        if i == (len(nums)-1):
+            if target > nums[i]:
+                return i+1
+        if nums[i] < target and target < nums[i+1]:
+            return i+1
+        if target < nums[0]:
+            return 0
+print(searchInsert(nums,target)) 
